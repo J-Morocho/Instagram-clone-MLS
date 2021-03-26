51c529c9ac5195f681642a539c311bb0ad312971
Code-33 <77262307+Code-33@users.noreply.github.com>
2021-03-26 14:46:08 -0400

    Create usersClass.js

---
+++ instagram-mini-project/controllers/usersClass.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+
+class usersInsta{
+   constructor ({id,name,username, email,avatar_url}){
+   this.id = id;
+   this.name = name;
+   this.username = username;
+   this.email = email;
+   this.avatar_url = avatar_url;
+   }
+//static async
+//return
+}
+
+module.exports = usersInsta;
