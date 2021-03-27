7f8e66d52d1f678334a71c09f7da994c158b4bfb
Code-33 <77262307+Code-33@users.noreply.github.com>
2021-03-27 14:55:11 -0400

    Update index.js
    separated table routes,
     fixed routes

--- instagram-mini-project/index.js
+++ instagram-mini-project/index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+////////////////////////////////Users Routes---------------------------------
 
 //Users get methods
 app.get('/api/users', users.all);
@@ -38,7 +39,10 @@ app.get('/api/users/email', users.emails);
 app.get('/api/users/id/:id', users.user_id);
 
 
-app.get('/api/users/username/:username', users.user_name);
+app.get('/api/users/name/:name', users.user_name);
+
+
+app.get('/api/users/username/:username', users.user_username);
 
 
 app.get('/api/users/email/:email', users.user_email);
@@ -51,8 +55,15 @@ app.get('/api/users/email/:email', users.user_email);
 //    "email":"bob@example.com",
 //    "avatar_url":"https://imgir.com/bobbybigs/sompic4"
 //}
-app.post('/api/users/', users.add_user);
+app.post('/api/users/add', users.add_user);
 
+
+//Users delete method
+app.delete('/api/users/delete/:id', users.delete_user);
+
+
+
+////////////////////////////////Posts Routes---------------------------------
 
 //Posts get methods
 app.get('/api/posts', posts.all);
@@ -61,10 +72,7 @@ app.get('/api/posts', posts.all);
 app.get('/api/posts/id/:id', posts.posts_id);
 
 
-app.get('/api/posts/users_id/:id', posts.posts_user_id);
-
-
-app.get('/api/posts/url/:url', posts.posts_url);
+app.get('/api/posts/users_id/:id', posts.posts_users_id);
 
 
 //Posts post method
@@ -73,33 +81,34 @@ app.get('/api/posts/url/:url', posts.posts_url);
 //    "photo_cap":"ex",
 //    "url":"ex.png"
 //}
-app.post('/api/posts/add_posts', posts.add_posts);
+app.post('/api/posts/add', posts.add_posts);
 
 
 //Posts delete method
-app.delete('/api/posts/delete_posts/:id', posts.delete_posts);
+app.delete('/api/posts/delete/:id', posts.delete_posts);
 
 
-//Posts put/patch method
-app.put('/api/posts/:id', posts.update_posts);
-app.patch('/api/posts/:id', posts.update_posts);
+//Posts put/patch method - updates photo_cap only
+app.put('/api/posts/update/:id', posts.update_posts);
+app.patch('/api/posts/update/:id', posts.update_posts);
+
 
 
 //Comments get method
 app.get('/api/comments', comments.all)
 
 
+//Get all comments from post id
 app.get('/api/post/:id/comments', comments.post_comments)
 
-app.post('/api/post/addcomment', comments.add_comment) 
 
+//Add a comment to post -----
+app.post('/api/post/addcomment', comments.add_comment)
+
+
+//Deleting the comment / sends post id and comment id----
 app.delete('/api/post/:id/comment/:comment_id', comments.delete_post_comment)
 
-
-//app.patch('/api/post/:id/comment/:comment_id', comments.edit_comment)
-
-//Comments delete method
-app.delete('/api/post/:id/comment/:comment_id', comments.delete_post_comment)
 
 
 //Comments put/patch method
